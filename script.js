c98c51930ceb1ceb1225561c0911b0df9e6bb025
MIYA2021 <80123162+MIYA2021@users.noreply.github.com>
2023-10-17 08:24:25 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,257 @@
+const questions = [
+    {
+        question: "ソアリン：ファンタスティック・フライトで最後に到着する場所といえばどこ？",
+        choices: ["東京ディズニーランド", "東京ディズニーシー", "東京スカイツリー"],
+        correctIndex: 1
+    },
+    {
+        question: "トイ・ストーリー3に出てくるスタジオジブリ作品のキャラクターは？",
+        choices: ["トトロ", "こだま", "カオナシ"],
+        correctIndex: 0
+    },
+    {
+        question: "ミッキーマウスとミニーマウスの誕生日は？",
+        choices: ["5/25", "9/5", "11/18"],
+        correctIndex: 2
+    },
+    {
+        question: "シンデレラの魔法使いがシンデレラに唱える呪文といえば",
+        choices: ["ピクデピクデプー", "ビビデバビデブー", "エクスペクトパトローナム"],
+        correctIndex: 1
+    },
+    {
+        question: "ミッキーの恋人は？",
+        choices: ["ミニーマウス", "デイジーダック", "ダッフィー"],
+        correctIndex: 0
+    },
+    {
+        question: "クマのプーさんが好きなのは？？",
+        choices: ["砂糖", "ハチミツ", "ハチの巣"],
+        correctIndex: 1
+    },
+    {
+        question: "エルサはどこの国の女王ですか",
+        choices: ["アレンディエール", "アレンデール", "アレンドゥール"],
+        correctIndex: 1
+    },
+    {
+        question: "ファインディング・ニモの元となった魚の種類は？",
+        choices: ["ミドリフエフキ", "ナンヨウハギ", "カクレクマノミ"],
+        correctIndex: 2
+    },
+    {
+        question: "シンデレラが乗っていた馬車はなんの食べ物？",
+        choices: ["りんご", "かぼちゃ", "メロン"],
+        correctIndex: 1
+    },
+    {
+        question: "白雪姫が悪いか魔女からもらって食べてしまったのは？",
+        choices: ["ドーナッツ", "みかん", "りんご"],
+        correctIndex: 2
+    },
+    {
+        question: "ウッディの靴に書いてある名前は？",
+        choices: ["ウッディの名前", "アンディの名前", "アンディの誕生日"],
+        correctIndex: 1
+    },
+    {
+        question: "ディズニーシーの入り口にある地球儀の名前は？",
+        choices: ["アクアスフィア", "ユートピア", "ディストピア"],
+        correctIndex: 0
+    },
+    {
+        question: "バズ・ライトイヤーの敵キャラといえばどれ？",
+        choices: ["ヘレックス", "ギーグ", "ザーグ"],
+        correctIndex: 2
+    },
+    {
+        question: "モンスターズインクに出てくる女の子の名前は？",
+        choices: ["ビー", "ブー", "ベー"],
+        correctIndex: 1
+    },
+    
+    {
+        question: "次のダッフィーレンズのうち女の子はどれ？",
+        choices: ["ステラ・ルー", "オル・メル", "ジェラトーニ"],
+        correctIndex: 0
+    }
+    {
+        question: "ホーンテッドマンションの館のなかにある時計は、12の部分はどれか？",
+        choices: ["15", "13", "19"],
+        correctIndex: 1
+    }
+    {
+        question: "ホーンテッドマンションがあるテーマランドは？",
+        choices: ["ファンタジーランド", "トゥモローランド", "ウエスタンランド"],
+        correctIndex: 0
+    }
+    {
+        question: "1番古いホーンテッドマンションがあるパークは？",
+        choices: ["カルフォルニア", "東京", "パリ"],
+        correctIndex: 2
+    }
+    {
+        question: "ホーンテッドマンションの待ち時間でないものは？",
+        choices: ["9分", "13分", "4分"],
+        correctIndex: 0
+    }
+    {
+        question: "ホーンテッドマンションの黒い乗り物の名前は？",
+        choices: ["ブラックバギー", "ドゥームバギー", "ゴーストバギー"],
+        correctIndex: 1
+    }
+    {
+        question: "ホーンテッドマンションには管理人がいるが、連れている動物は何か",
+        choices: ["猫", "犬", "鳥"],
+        correctIndex: 1
+    }
+    {
+        question: "ホーンテッドマンションの入り口は何口か。",
+        choices: ["正門入り口", "庭の入り口", "使用人出入り口"],
+        correctIndex: 2
+    }
+    {
+        question: "ホーンテッドマンションのアトラクション乗り場のベルトコンベアにあるイラストは何か。",
+        choices: ["コウモリ", "蜘蛛", "スカル"],
+        correctIndex: 0
+    }
+    {
+        question: "ホーンテッドマンションには隠れたキャラクターがいるが、ないものはどれか。",
+        choices: ["ミッキー", "ミニー", "ドナルド"],
+        correctIndex: 2
+    }
+    {
+        question: "ホーンテッドマンションには隠れたキャラクターがいるが、ないものはどれか。",
+        choices: ["ミッキー", "ミニー", "ドナルド"],
+        correctIndex: 2
+    }
+    {
+        question: "シンデレラ城の屋根の先端は何色？",
+        choices: ["白", "金", "青"],
+        correctIndex: 1
+    }
+    {
+        question: "東京ディズニーランドの敷地はオープン以前はどんな場所だった？",
+        choices: ["公園", "ゴミ処理場", "海"],
+        correctIndex: 1
+    }
+    {
+        question: "くまのプーさんに出てくる豚のキャラクターの名前は？",
+        choices: ["ピグレット", "ティガー", "イーヨー"],
+        correctIndex: 0
+    }
+    {
+        question: "くまのプーさんに出てくる豚のキャラクターの名前は？",
+        choices: ["ピグレット", "ティガー", "イーヨー"],
+        correctIndex: 0
+    }
+    {
+        question: "ピグレットの好物は？",
+        choices: ["アザミの花", "どんぐり", "プーさんと同じハチミツ"],
+        correctIndex: 1
+    }
+    {
+        question: "ピグレットの好物は？",
+        choices: ["アザミの花", "プーさんと同じはちみつ", "どんぐり"],
+        correctIndex: 2
+    }
+    {
+        question: "リスのキャラクターチップとデールの、デールの鼻は何色でしょう？",
+        choices: ["黒", "黄色", "赤"],
+        correctIndex: 2
+    }
+    {
+        question: "ファインディングニモのお父さんの名前は？",
+        choices: ["マーリン", "マリン", "クラッシュ"],
+        correctIndex: 0
+    }
+    {
+        question: "アラジンに出てくる魔法のランプで叶えられる願いは？",
+        choices: ["死人を生き返らせる", "王子になれる", "自分のことを好きにさせることが出来る"],
+        correctIndex: 1
+    }
+    {
+        question: "トイストーリー3に出てくるロッツォはハグするとどんな匂いがする？",
+        choices: ["せっけん", "いちご", "バラ"],
+        correctIndex: 1
+    }
+    {
+        question: "ホーンテッドマンションに住んでいる幽霊の数は？",
+        choices: ["10", "555", "999"],
+        correctIndex: 2
+    }
+    {
+        question: "ディズニーランドのクリッターカントリーにあるレストランの名前は？",
+        choices: ["グラン・マサラ", "グランマ・サラ", "グランマサ・ラ"],
+        correctIndex: 1
+    }
+    {
+        question: "次のホテルのうち、ディズニーホテルはどれ？",
+        choices: ["セレブレーションホテル", "シェラトン・グランデトーキョーベイホテル", "オリエンタルホテル東京ベイ"],
+        correctIndex: 0
+    }
+    {
+        question: "ピノキオを作ったおじいさんの名前は",
+        choices: ["ピボット", "ゼペット", "サラット"],
+        correctIndex: 1
+    }
+    {
+        question: "では、その黒いライドは、ホリデーナイトメアの期間中は何と呼ばれるか",
+        choices: ["黒のソリ", "闇のソリ", "やばいソリ"],
+        correctIndex: 0
+    }
+    {
+        question: "ホーンテッドマンションでゲストを案内するゴーストの名は？",
+        choices: ["グリニングゴースト", "マダムレオタ", "ゴーストホスト"],
+        correctIndex: 0
+    }
+
+];
+
+let currentQuestionIndex;
+
+function initQuiz() {
+    currentQuestionIndex = -1; // 最初に次の質問に進む前に1回インクリメントされるので-1から始めます
+    nextQuestion();
+}
+
+function nextQuestion() {
+    // ランダムに次の質問のインデックスを選択
+    const randomIndex = Math.floor(Math.random() * questions.length);
+
+    // 同じ問題が連続して出ないようにする
+    currentQuestionIndex = (randomIndex !== currentQuestionIndex) ? randomIndex : (randomIndex + 1) % questions.length;
+
+    // 質問を表示
+    displayQuestion();
+}
+
+function displayQuestion() {
+    const currentQuestion = questions[currentQuestionIndex];
+    document.getElementById('question').textContent = currentQuestion.question;
+
+    const choices = document.getElementsByClassName('choice');
+    for (let i = 0; i < choices.length; i++) {
+        choices[i].textContent = currentQuestion.choices[i];
+    }
+}
+
+function checkAnswer(choiceIndex) {
+    const currentQuestion = questions[currentQuestionIndex];
+    if (choiceIndex === currentQuestion.correctIndex) {
+        alert("正解！");
+    } else {
+        alert("不正解...");
+    }
+
+    // 次の質問に進む
+    nextQuestion();
+}
+
+function resetQuiz() {
+    initQuiz();
+}
+
+// クイズ初期化
+initQuiz();
+// JavaScript Document
