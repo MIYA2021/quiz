6535c22d4acb8f658df0a72bd2df6e69974b84f8
MIYA2021 <80123162+MIYA2021@users.noreply.github.com>
2023-10-17 09:01:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -74,138 +74,137 @@ const questions = [
         question: "次のダッフィーレンズのうち女の子はどれ？",
         choices: ["ステラ・ルー", "オル・メル", "ジェラトーニ"],
         correctIndex: 0
-    }
+    },
     {
         question: "ホーンテッドマンションの館のなかにある時計は、12の部分はどれか？",
         choices: ["15", "13", "19"],
         correctIndex: 1
-    }
+    },
     {
         question: "ホーンテッドマンションがあるテーマランドは？",
         choices: ["ファンタジーランド", "トゥモローランド", "ウエスタンランド"],
         correctIndex: 0
-    }
+    },
     {
         question: "1番古いホーンテッドマンションがあるパークは？",
         choices: ["カルフォルニア", "東京", "パリ"],
         correctIndex: 2
-    }
+    },
     {
         question: "ホーンテッドマンションの待ち時間でないものは？",
         choices: ["9分", "13分", "4分"],
         correctIndex: 0
-    }
+    },
     {
         question: "ホーンテッドマンションの黒い乗り物の名前は？",
         choices: ["ブラックバギー", "ドゥームバギー", "ゴーストバギー"],
         correctIndex: 1
-    }
+    },
     {
         question: "ホーンテッドマンションには管理人がいるが、連れている動物は何か",
         choices: ["猫", "犬", "鳥"],
         correctIndex: 1
-    }
+    },
     {
         question: "ホーンテッドマンションの入り口は何口か。",
         choices: ["正門入り口", "庭の入り口", "使用人出入り口"],
         correctIndex: 2
-    }
+    },
     {
         question: "ホーンテッドマンションのアトラクション乗り場のベルトコンベアにあるイラストは何か。",
         choices: ["コウモリ", "蜘蛛", "スカル"],
         correctIndex: 0
-    }
+    },
     {
         question: "ホーンテッドマンションには隠れたキャラクターがいるが、ないものはどれか。",
         choices: ["ミッキー", "ミニー", "ドナルド"],
         correctIndex: 2
-    }
+    },
     {
         question: "ホーンテッドマンションには隠れたキャラクターがいるが、ないものはどれか。",
         choices: ["ミッキー", "ミニー", "ドナルド"],
         correctIndex: 2
-    }
+    },
     {
         question: "シンデレラ城の屋根の先端は何色？",
         choices: ["白", "金", "青"],
         correctIndex: 1
-    }
+    },
     {
         question: "東京ディズニーランドの敷地はオープン以前はどんな場所だった？",
         choices: ["公園", "ゴミ処理場", "海"],
         correctIndex: 1
-    }
+    },
     {
         question: "くまのプーさんに出てくる豚のキャラクターの名前は？",
         choices: ["ピグレット", "ティガー", "イーヨー"],
         correctIndex: 0
-    }
+    },
     {
         question: "くまのプーさんに出てくる豚のキャラクターの名前は？",
         choices: ["ピグレット", "ティガー", "イーヨー"],
         correctIndex: 0
-    }
+    },
     {
         question: "ピグレットの好物は？",
         choices: ["アザミの花", "どんぐり", "プーさんと同じハチミツ"],
         correctIndex: 1
-    }
+    },
     {
         question: "ピグレットの好物は？",
         choices: ["アザミの花", "プーさんと同じはちみつ", "どんぐり"],
         correctIndex: 2
-    }
+    },
     {
         question: "リスのキャラクターチップとデールの、デールの鼻は何色でしょう？",
         choices: ["黒", "黄色", "赤"],
         correctIndex: 2
-    }
+    },
     {
         question: "ファインディングニモのお父さんの名前は？",
         choices: ["マーリン", "マリン", "クラッシュ"],
         correctIndex: 0
-    }
+    },
     {
         question: "アラジンに出てくる魔法のランプで叶えられる願いは？",
         choices: ["死人を生き返らせる", "王子になれる", "自分のことを好きにさせることが出来る"],
         correctIndex: 1
-    }
+    },
     {
         question: "トイストーリー3に出てくるロッツォはハグするとどんな匂いがする？",
         choices: ["せっけん", "いちご", "バラ"],
         correctIndex: 1
-    }
+    },
     {
         question: "ホーンテッドマンションに住んでいる幽霊の数は？",
         choices: ["10", "555", "999"],
         correctIndex: 2
-    }
+    },
     {
         question: "ディズニーランドのクリッターカントリーにあるレストランの名前は？",
         choices: ["グラン・マサラ", "グランマ・サラ", "グランマサ・ラ"],
         correctIndex: 1
-    }
+    },
     {
         question: "次のホテルのうち、ディズニーホテルはどれ？",
         choices: ["セレブレーションホテル", "シェラトン・グランデトーキョーベイホテル", "オリエンタルホテル東京ベイ"],
         correctIndex: 0
-    }
+    },
     {
         question: "ピノキオを作ったおじいさんの名前は",
         choices: ["ピボット", "ゼペット", "サラット"],
         correctIndex: 1
-    }
+    },
     {
         question: "では、その黒いライドは、ホリデーナイトメアの期間中は何と呼ばれるか",
         choices: ["黒のソリ", "闇のソリ", "やばいソリ"],
         correctIndex: 0
-    }
+    },
     {
         question: "ホーンテッドマンションでゲストを案内するゴーストの名は？",
         choices: ["グリニングゴースト", "マダムレオタ", "ゴーストホスト"],
         correctIndex: 0
-    }
-
+    },
 ];
 
 let currentQuestionIndex;
